300c2753fb33d806539858e04dde42bd76acac4e
xuyecan <xuyecan@gmail.com>
2024-07-17 23:25:18 +0800

    Initial Commit

---
+++ AudioSwitcher/AudioSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioSwitcherApp.swift
+//  AudioSwitcher
+//
+//  Created by xuyecan on 2024/7/17.
+//
+
+import SwiftUI
+
+@main
+struct AudioSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
